71e80a48e15f1c836994f7fd64c870be5bba6d12
Niklas Zimmer <niklaszimmer2902@gmail.com>
2021-10-27 18:22:32 +0200

    Dont wipe build dir when existing

--- cmd_test.py
+++ cmd_test.py
@@ -1,6 +1,7 @@
 import subprocess, time
 
 import main
+import os
 from main import *
 from Cython.Build import cythonize
 from meson_scripts import copy_tools, download_python
@@ -42,6 +43,8 @@ my_parser.add_argument('--target_platform',
 # Execute parse_args()
 args = my_parser.parse_args()
 
+build_dir = f"build_meson/{args.target_platform}"
+
 
 
 start = time.time()
@@ -56,8 +59,8 @@ cythonize_files()
 
 res = subprocess.Popen("vcvarsall.bat x86_amd64 "
                  "& cl"
-                 f"& meson build_meson/{args.target_platform} --cross-file platforms/{args.target_platform}.cross "
-                 f"--cross-file platforms/compilers/{args.compiler}_compiler.native --buildtype=release --wipe"
+                 f"& meson {build_dir} --cross-file platforms/{args.target_platform}.cross "
+                 f"--cross-file platforms/compilers/{args.compiler}_compiler.native --buildtype=release {'--wipe' if os.path.isdir(build_dir) else ''}"
                  f"& ninja -C build_meson/{args.target_platform}", shell=True)
 
 res.wait()
