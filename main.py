f6f79973734c8c8df671737eee92b5fb63b39e5f
Niklas Zimmer <niklaszimmer2902@gmail.com>
2022-01-25 20:32:47 +0100

    Fix some singletons. Fix setting python home in c

--- main.py
+++ main.py
@@ -208,16 +208,12 @@ def generate_classes(obj):
     #TODO:Try to improve this
     if(obj["singleton"]):
         result += f"""  @staticmethod\n"""
-        result += f"""  def _new():\n"""
+        #TODO: How to handle class Input
+        result += f"""  def instance():\n"""
         result += f"""      cdef char* name = <char*> "{obj["name"].strip('_')}"\n"""
-        if(obj["name"].startswith("_")):
-            result += f"""      cdef {obj['name']} obj = <{obj['name']}>{obj['name']}()\n"""
-            result += f"""      obj.godot_owner = api_core.godot_global_get_singleton(name)\n"""
-            result += f"""      return obj\n"""
-        else:
-            #Todo: check why I have to make a difference here
-            result += f"""      cdef {obj['name']} obj = <{obj['name']}> nativescript_api_11.godot_nativescript_get_instance_binding_data(language_index,api_core.godot_get_class_constructor(name)())\n"""
-            result += f"""      return obj\n"""
+        result += f"""      cdef {obj['name']} obj = <{obj['name']}>{obj['name']}()\n"""
+        result += f"""      obj.godot_owner = api_core.godot_global_get_singleton(name)\n"""
+        result += f"""      return obj\n"""
     result += generate_properties(obj)
     results = generate_methods(obj, import_string)
     result += results[0]
@@ -268,7 +264,7 @@ def generate_singleton(obj):
     @classmethod
     def instance(cls):
         if not cls._instance:
-            cls._instance = {obj['name']}._new()
+            cls._instance = {obj['name']}.instance()
         return cls._instance\n\n"""
 
 # The import string at the start of the file
--- py4godot/godot_bindings/main.c
+++ py4godot/godot_bindings/main.c
@@ -22,6 +22,19 @@
 #include "../gdnative_api/api_api.h"
 #include <string.h>
 
+#ifdef _WIN64
+#define PYTHONHOME L"addons/windows64/cpython-3.9.7-windows64/python/install"
+
+#elif _WIN32
+#define PYTHONHOME L"addons/windows64/cpython-3.9.7-windows32/python/install"
+
+#elif __LINUX__
+#define PYTHONHOME L"addons/windows64/cpython-3.9.7-windows32/python/install"
+
+#elif __APPLE__
+#define PYTHONHOME L"addons/windows64/cpython-3.9.7-windows32/python/install"
+#endif
+
 
 typedef struct user_data_struct {
 	char data[256];
@@ -125,7 +138,7 @@ void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *p_options) {
 		};
 	};
     Py_SetProgramName(L"godot");
-    Py_SetPythonHome(L"addons/windows64/cpython-3.9.7-windows64/python/install");
+    Py_SetPythonHome(PYTHONHOME);
     // Initialize interpreter but skip initialization registration of signal handlers
     Py_InitializeEx(0);
     // PyEval_InitThreads acquires the GIL, so we must release it later.
