9c341e69f533d1c58a8fc1de194c8007c32ad120
Niklas Zimmer <niklaszimmer2902@gmail.com>
2022-01-29 15:51:06 +0100

    test subprocess

--- cmd_test.py
+++ cmd_test.py
@@ -77,6 +77,8 @@ compile_python_ver_file(current_platform)
 # initializing for msvc if wanted as compiler (todo:should be improved sometime)
 msvc_init = f"vcvarsall.bat {'x86_amd64'} & cl & " if "msvc" in args.compiler else ""
 
+res = subprocess.Popen("meson")
+"""
 res = subprocess.Popen(msvc_init +
                        f"meson {build_dir} --cross-file platforms/{args.target_platform}.cross "
                        f"--cross-file platforms/compilers/{args.compiler}_compiler.native "
@@ -84,6 +86,7 @@ res = subprocess.Popen(msvc_init +
                        f"--buildtype=release {'--wipe' if os.path.isdir(build_dir) else ''}"
                        #f"& ninja -C build_meson/{args.target_platform}"
                        , shell=True)
+"""
 
 res.wait()
 copy_tools.run(args.target_platform)
