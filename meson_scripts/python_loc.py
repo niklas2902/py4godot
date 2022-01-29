ecd0f8b2bac80704f3187383ea7b7fb1d734496d
Niklas Zimmer <niklaszimmer2902@gmail.com>
2022-01-29 16:41:25 +0100

    fix build

--- meson_scripts/python_loc.py
+++ meson_scripts/python_loc.py
@@ -1,4 +1,4 @@
 import os
 
 def get_python_dir(platform):
-    return (os.getcwd()+f"/python_files/cpython-3.9.7-{platform}/python/install/python.{'.exe' if 'windows' in platform else '' }").replace("\\", "/")
+    return (os.getcwd()+f"/python_files/cpython-3.9.7-{platform}/python/install/python{'.exe' if 'windows' in platform else '' }").replace("\\", "/")
