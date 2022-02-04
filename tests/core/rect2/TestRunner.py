457b6ed2f3102daddb2ff9a693d60bb5348d6730
Niklas Zimmer <niklaszimmer2902@gmail.com>
2022-02-04 19:19:59 +0100

    Fix indentations. Fix print tools

--- tests/core/rect2/TestRunner.py
+++ tests/core/rect2/TestRunner.py
@@ -6,17 +6,17 @@ import os
 
 @gdclass
 class TestRunner(Spatial):
-	
-	def __init__(self):
-		print("__init__")
-	@gdmethod
-	def _ready(self):
-		print("##########start#############")
-		suite = unittest.TestLoader().loadTestsFromModule(PythonTest)
-		res = unittest.TextTestRunner().run(suite)
-		if(len(res.failures) == 0):
-			self.get_tree().quit(0)
-		else:
-			self.get_tree().quit(1)
+
+    def __init__(self):
+        print("__init__")
+    @gdmethod
+    def _ready(self):
+        print("##########start#############")
+        suite = unittest.TestLoader().loadTestsFromModule(PythonTest)
+        res = unittest.TextTestRunner().run(suite)
+        if(len(res.failures) == 0):
+            self.get_tree().quit(0)
+        else:
+            self.get_tree().quit(1)
 
 
