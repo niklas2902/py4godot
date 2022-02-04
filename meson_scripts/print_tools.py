9cc91f6f84982712fa59c1dec6e71f201e3a9655
Niklas Zimmer <niklaszimmer2902@gmail.com>
2022-02-04 19:19:59 +0100

    Fix indentations. Fix print tools

--- meson_scripts/print_tools.py
+++ meson_scripts/print_tools.py
@@ -1,3 +1,6 @@
 def print_meson(platform):
-    with open (f"build_meson/{platform}/meson-logs/testlog.txt") as file:
-        print(file.read())
+    try:
+        with open (f"build_meson/{platform}/meson-logs/testlog.txt") as file:
+            print(file.read())
+    except Exception as e:
+        print(f"Exception occurred:{e.with_traceback()}")
