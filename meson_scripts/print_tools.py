f06e8e42b12323d6227a8119b3e0550abfec06ed
Niklas Zimmer <niklaszimmer2902@gmail.com>
2022-02-05 13:59:23 +0100

    Add more printing

--- meson_scripts/print_tools.py
+++ meson_scripts/print_tools.py
@@ -1,7 +1,10 @@
 def print_meson(platform):
     print("print _meson")
     try:
+        print("try printing")
         with open (f"build_meson/{platform}/meson-logs/testlog.txt") as file:
+            print(file)
             print(file.read())
     except Exception as e:
         print(f"Exception occurred:{e.with_traceback()}")
+    print("end printing")
