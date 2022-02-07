6a6bffe5c0cdf2a9f63114aa7b721cce14f10c2a
Niklas Zimmer <niklaszimmer2902@gmail.com>
2022-02-07 17:34:41 +0100

    allow printing tree

--- cmd_test.py
+++ cmd_test.py
@@ -113,7 +113,6 @@ if should_run_tests:
     res = subprocess.Popen(
         f"meson test -C build_meson/{args.target_platform}", shell=True)
     res.wait()
-    print(res.communicate())
 
     print("=================================Tests finished==================================")
     print("Running tests took:", time.time() - start, "seconds")
--- meson_scripts/print_tools.py
+++ meson_scripts/print_tools.py
@@ -1,6 +1,19 @@
+import glob
+
 def print_meson(platform):
     try:
         with open (f"build_meson/{platform}/meson-logs/testlog.txt") as file:
             print(file.read())
     except Exception as e:
         print(f"Exception occurred:{e.with_traceback()}")
+
+def print_tree(path, layer, max_layer=3):
+    if layer >= max_layer:
+        return
+    l = glob.glob(f"{path}/*", recursive=True)
+    for i in l:
+        print(layer *2* " "+i.replace(path+"\\", ""))
+        print_tree(i, layer + 1, max_layer)
+
+if __name__=="__main__":
+    print_tree("build_meson", 0)
