1cbd84bf36f32ed8ea786ecbdc7b3f0897a91a98
Niklas Zimmer <niklaszimmer2902@gmail.com>
2022-02-28 20:14:50 +0100

    Edit binary

--- meson_scripts/locations.py
+++ meson_scripts/locations.py
@@ -4,6 +4,6 @@ def get_python_dir(platform):
     if("windows" in platform):
         return (os.getcwd()+f"/python_files/cpython-3.9.7-{platform}/python/install/python.exe").replace("\\", "/")
     elif("linux" in platform):
-        return (os.getcwd() + f"/python_files/cpython-3.9.7-{platform}/python/install/bin/python").replace("\\", "/")
+        return (os.getcwd() + f"/python_files/cpython-3.9.7-{platform}/python/install/bin/python3").replace("\\", "/")
 def get_godot_dir(platform):
     return (os.getcwd()+f"/godot/Godot_v3.4.2-stable_win64.exe")
