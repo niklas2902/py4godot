30d606d04d5d782caccedf1367fc775cd624e7cc
Niklas <nzimmer@stud.hs-heilbronn.de>
2022-06-27 18:48:31 +0200

    Fix build

--- build.py
+++ build.py
@@ -9,7 +9,7 @@ from meson_scripts import copy_tools, download_python, generate_init_files, \
     locations, platform_check, generate_godot, \
     download_godot
 
-#generate_bindings.build()
+generate_bindings.build()
 
 
 def cythonize_files():
