0c583a4c43e1436a5236ee78a261156cfb173be7
niklas <niklaszimmer2902@gmail.com>
2022-11-13 22:46:11 +0100

    Furhter improve api

--- py4godot/pluginscript_api/api4.pyx
+++ py4godot/pluginscript_api/api4.pyx
@@ -5,7 +5,7 @@ from py4godot_core_holder.core_holder cimport *
 #print("hallo, ich bin importiert")
 cdef GDNativeInterface* _interface = get_interface()
 cdef api void init_py_language() with gil:
-    #print_warning ("init_py_langauge")
+    print_warning ("init_py_langauge")
     _interface.print_warning("-------------------test", "hallo", "hallo", 1)
     #cdef Engine engine = Engine()
     print_warning("-------------------after constructing engine")
--- py4godot/utils/print_tools.pyx
+++ py4godot/utils/print_tools.pyx
@@ -5,12 +5,15 @@ from py4godot_core_holder.core_holder cimport *
 cdef GDNativeInterface* gdnative_interface = get_interface()
 
 def print_warning(*objects, sep=' ', end=''):
-    cdef char * string = ""
+    gdnative_interface.print_warning("print_warning", "test", "test",1);
+    cdef str string = ""
     for object in objects:
         string += str(object) + sep
     string.rstrip(sep)
     string += end
-    gdnative_interface.print_warning(string, "test", "test",1);
+    b_str = string.encode('utf-8')
+    cdef char* c_str = b_str
+    gdnative_interface.print_warning(c_str, "test", "test",1);
 
 def print(*objects, sep=' ', end=''):
     string = ""
