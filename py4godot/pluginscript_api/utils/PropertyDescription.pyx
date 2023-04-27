ac0434325678da8cff2f063a0965956f98201f2b
niklas <niklaszimmer2902@gmail.com>
2023-04-27 21:42:47 +0200

    Allow more values for properties

--- py4godot/pluginscript_api/utils/PropertyDescription.pyx
+++ py4godot/pluginscript_api/utils/PropertyDescription.pyx
@@ -1,6 +1,7 @@
 from py4godot.pluginscript_api.hints.BaseHint cimport *
 from py4godot.godot_bindings.binding4_godot4 cimport *
 from py4godot.classes.generated4_core cimport *
+from py4godot.classes.Object.Object cimport *
 from py4godot.utils.utils cimport *
 from py4godot.utils.print_tools import *
 
@@ -16,7 +17,45 @@ property.hint_string = hint.godot_owner
 property.usage = 6
 
 
-cdef dict types_dict = {int:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_INT, str:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_STRING}
+cdef dict types_dict = {
+int:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_INT,
+str:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_STRING,
+type(True):GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_BOOL,
+float:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_FLOAT,
+Object:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_OBJECT,
+Vector4:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_VECTOR4,
+PackedColorArray:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_PACKED_COLOR_ARRAY,
+Vector2i:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_VECTOR2I,
+Vector3i:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_VECTOR3I,
+Quaternion:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_QUATERNION,
+Callable:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_CALLABLE,
+PackedVector2Array:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_PACKED_VECTOR2_ARRAY,
+Vector4i:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_VECTOR4I,
+Array:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_ARRAY,
+PackedByteArray:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_PACKED_BYTE_ARRAY,
+AABB:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_AABB,
+Transform3D:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_TRANSFORM3D,
+Transform2D:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_TRANSFORM2D,
+Rect2i:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_RECT2I,
+PackedFloat64Array:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_PACKED_FLOAT64_ARRAY,
+Dictionary:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_DICTIONARY,
+PackedStringArray:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_PACKED_STRING_ARRAY,
+Plane:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_PLANE,
+PackedInt64Array:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_PACKED_INT64_ARRAY,
+Signal:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_SIGNAL,
+PackedFloat32Array:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_PACKED_FLOAT32_ARRAY,
+Rect2:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_RECT2,
+Vector2:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_VECTOR2,
+NodePath:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_NODE_PATH,
+Color:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_COLOR,
+StringName:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_STRING_NAME,
+RID:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_RID,
+Projection:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_PROJECTION,
+PackedInt32Array:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_PACKED_INT32_ARRAY,
+Vector3:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_VECTOR3,
+PackedVector3Array:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_PACKED_VECTOR3_ARRAY,
+Basis:GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_BASIS,
+}
 
 cdef object convert_val(object val):
     if(type(val) == str):
@@ -27,7 +66,7 @@ cdef GDExtensionVariantType get_variant_type(object type_):
     if(type_ in types_dict.keys()):
         return types_dict[type_]
     print_error("couldn't find type", type_)
-    return GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_INT
+    return GDExtensionVariantType.GDEXTENSION_VARIANT_TYPE_NIL
 
 cdef class PropertyDescription:
     """"Description class for the properties, a gdclass can have and which can be found in the editor"""
