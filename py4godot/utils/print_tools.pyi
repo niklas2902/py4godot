d57f25361b387c178cc53928ce28f82334054009
Niklas Zimmer <nzimmer@uni-bremen.de>
2023-11-16 17:28:07 +0100

    Add a missing import

---
+++ py4godot/utils/print_tools.pyi
@@ -0,0 +1,7 @@
+def print_error(*objects, sep=' ', end='') ->None :pass
+
+def error(*objects, sep=' ', end='') -> None :pass
+
+def debug(*objects, sep=' ', end='') -> None:pass
+
+def print(*objects, sep=' ', end='') ->None :pass
