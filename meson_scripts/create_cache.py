ade3febb9720745ce1ac57a914e9eb13fd5030db
Niklas Zimmer <nzimmer@uni-bremen.de>
2024-09-17 00:07:10 +0200

    Fixed scripts

--- meson_scripts/create_cache.py
+++ meson_scripts/create_cache.py
@@ -1,1 +1,1 @@
-with open("../py4godot/classes/cache", "w"):pass
+with open("py4godot/classes/cache", "w"):pass
--- meson_scripts/does_cache_exist.py
+++ meson_scripts/does_cache_exist.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-if os.path.isfile("../py4godot/classes/cache"):
+if os.path.isfile("py4godot/classes/cache"):
     print("Cache exists")
     sys.exit(0)
 else:
