0c468fb7cb4487e9d9ee837f5f58f40228fdb928
Niklas Zimmer <nzimmer@uni-bremen.de>
2024-09-17 17:02:00 +0200

    trying to fix

--- cythonize_files.py
+++ cythonize_files.py
@@ -2,16 +2,17 @@ import argparse
 import meson_scripts.cythonize_files.cythonize_files as full_cythonize
 import meson_scripts.cythonize_files.dev_cythonize_files as dev_cythonize
 import meson_scripts.cythonize_files.cache_hit_cythonize as cache_cythonize
-my_parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
-my_parser.add_argument('-mode', default="release",
-                       help='Sepcify whether you want to turn all files to c++ files (release) or only a smaller portion (dev).'
-                            'This impacts mainly the speed')
-# Execute parse_args()
-args = my_parser.parse_args()
+if __name__ == "__main__":
+    my_parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
+    my_parser.add_argument('-mode', default="release",
+                           help='Sepcify whether you want to turn all files to c++ files (release) or only a smaller portion (dev).'
+                                'This impacts mainly the speed')
+    # Execute parse_args()
+    args = my_parser.parse_args()
 
-if args.mode == "release":
-    full_cythonize.main()
-elif args.mode == "dev":
-    dev_cythonize.main()
-elif args.mode == "cache":
-    cache_cythonize.main()
+    if args.mode == "release":
+        full_cythonize.main()
+    elif args.mode == "dev":
+        dev_cythonize.main()
+    elif args.mode == "cache":
+        cache_cythonize.main()
