87ad38c16eba4e69ccac635671c1b93e07c4c3b3
Niklas Zimmer <nzimmer@uni-bremen.de>
2025-03-23 22:27:20 +0100

    Automatically detect godot

--- meson_scripts/locations.py
+++ meson_scripts/locations.py
@@ -1,4 +1,5 @@
 import os, json
+import shutil
 
 with open('config.json', 'r') as f:
     config_data = json.load(f)
@@ -15,9 +16,6 @@ def get_python_dir(platform):
     elif "darwin" in platform:
         return (os.getcwd() + f"/{python_dir}/{python_ver}-{platform}/python/bin/python3").replace("\\", "/")
 def get_godot_dir(platform):
-    if "windows" in platform:
-        return (os.getcwd()+f"/{dir_godot_extract_to}/Godot_v4.3-stable_win64.exe")
-    elif "linux" in platform:
-        return (os.getcwd()+f"/{dir_godot_extract_to}/Godot_v4.3-stable_linux.x86_64")
-    elif "darwin" in platform:
-        return (os.getcwd()+f"/{dir_godot_extract_to}/Godot_v3.4.3-stable_x11.64")
+        godot_path = shutil.which("godot")
+        if godot_path:
+            return godot_path
