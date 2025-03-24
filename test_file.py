5ba594635ee33e84ea6cc6fe9803af7d68554619
Niklas Zimmer <nzimmer@uni-bremen.de>
2025-03-24 17:21:37 +0100

    Fixed output

--- test_file.py
+++ test_file.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import sys
 import argparse
@@ -15,16 +16,27 @@ command = args.godot
 project_path = args.path
 command_args = [command, "--path", project_path]
 command_args += ["--headless"]
-# Run the command
+project = os.path.basename(project_path)
+stdout_log = open(f"build/{project}_godot_stdout.log", "w")
+stderr_log = open(f"build/{project}_godot_stderr.log", "w")
 try:
     print("project_path:", project_path)
     print("command_args:", command_args)
-    result = subprocess.run(command_args, check=True)
+    result = subprocess.run(command_args,
+                            stdout=stdout_log,
+                            stderr=stderr_log,
+                            check=True, text=True)
     print(f"Command executed successfully with return code: {result.returncode}")
-    sys.exit(0)
 except subprocess.CalledProcessError as e:
     print(f"Command failed with return code: {e.returncode}")
     sys.exit(1)
 except FileNotFoundError:
     print("The specified file or command was not found.", project_path, command_args)
     sys.exit(2)
+except Exception as e:
+    print(str(e))
+    sys.exit(3)
+
+stdout_log.close()
+stderr_log.close()
+sys.exit(0)
