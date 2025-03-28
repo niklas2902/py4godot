45104eb315300d2c84a8cc45c9eef8ac29e7b966
Niklas Zimmer <nzimmer@uni-bremen.de>
2025-03-28 22:13:25 +0100

    Added more tests for numpy

--- tests/libraries/numpy/PythonTest.py
+++ tests/libraries/numpy/PythonTest.py
@@ -5,4 +5,35 @@ from py4godot.classes.core import *
 class PythonTest(unittest.TestCase):
 
     def test_import(self):
-        import numpy
+        """Test that NumPy can be imported."""
+        try:
+            import numpy
+        except ImportError:
+            self.fail("NumPy could not be imported")
+
+    def test_array_creation(self):
+        """Test that a NumPy array can be created."""
+        import numpy as np
+        arr = np.array([1, 2, 3])
+        self.assertEqual(arr.tolist(), [1, 2, 3])
+
+    def test_basic_operations(self):
+        """Test that NumPy can perform basic arithmetic operations."""
+        import numpy as np
+        arr = np.array([1, 2, 3])
+        result = arr + 1
+        self.assertTrue((result == np.array([2, 3, 4])).all())
+
+    def test_matrix_multiplication(self):
+        """Test that NumPy can perform matrix multiplication."""
+        import numpy as np
+        A = np.array([[1, 2], [3, 4]])
+        B = np.array([[2, 0], [1, 2]])
+        result = np.matmul(A, B)
+        expected = np.array([[4, 4], [10, 8]])
+        self.assertTrue((result == expected).all())
+
+    def test_numpy_version(self):
+        """Test that NumPy has a valid version number."""
+        import numpy as np
+        self.assertIsInstance(np.__version__, str)
