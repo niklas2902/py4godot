1a1d92e54e76d3b6e372e715c9b2b0eb12232e79
Niklas Zimmer <nzimmer@uni-bremen.de>
2025-04-04 09:24:51 +0200

    #111: Fixed color test

--- tests/core/color/PythonTest.py
+++ tests/core/color/PythonTest.py
@@ -8,4 +8,4 @@ class PythonTest(unittest.TestCase):
         c = Color.new3(0.1,0.5,0.8)
         self.assertAlmostEqual(c.r, 0.1, places=4)
         self.assertAlmostEqual(c.g, 0.5, places=4)
-        self.assertAlmostEqual(c.b, 0.6, places=4)
+        self.assertAlmostEqual(c.b, 0.8, places=4)
