91e7f94d08d36886981a7d2297ec7cb8bd83605e
Niklas Zimmer <nzimmer@uni-bremen.de>
2025-04-14 21:51:10 +0200

    Fixed error by using raw strings

--- py4godot/script_extension/PyScriptExtension.cpp
+++ py4godot/script_extension/PyScriptExtension.cpp
@@ -122,8 +122,8 @@ void init_pluginscript_api(){
     // Convert parentDir to Python string format
     std::string pythonCode = "import sys\n"
                              "import os\n"
-                             "sys.path.append('''" + std::string{ cwd} + "''')\n"
-                             "sys.path.append(''' " + std::string{cwd} + std::string{PYTHONPATH} + "''')";
+                             "sys.path.append(r'''" + std::string{ cwd} + "''')\n"
+                             "sys.path.append(r''' " + std::string{cwd} + std::string{PYTHONPATH} + "''')";
     // Convert Python code to const char* for PyRun_SimpleString
     const char *pythonCodeWchar = pythonCode.c_str();
 
