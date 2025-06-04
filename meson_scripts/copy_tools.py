5a7f4db7d733c388de17b25c1b01a18cfee3fb44
niklas <niklas-zimmer@hotmail.com>
2025-06-04 18:07:36 +0200

    Fixed stripping platform

--- meson_scripts/copy_tools.py
+++ meson_scripts/copy_tools.py
@@ -13,7 +13,8 @@ with open('config.json', 'r') as f:
 
 def strip_platform(text):
     text = text[1:]
-    return text.lstrip("linux64").lstrip("windows64").lstrip("windows32").lstrip("linux32").lstrip("darwin64")
+    return text.lstrip("linux64").lstrip("windows64").lstrip("windows32").lstrip("linux32").lstrip("darwin64").lstrip(
+        "linuxarm64").lstrip("windowsarm64")
 
 
 def run(platform):
