0783cc801039714024483851e642c74851f32456
niklas <niklas-zimmer@hotmail.com>
2025-07-28 10:28:57 +0200

    Fixed utils types

--- py4godot/utils/utils.pyi
+++ py4godot/utils/utils.pyi
@@ -1,7 +1,7 @@
-from py4godot.classes.Node.Node import *
-import py4godot.classes.Viewport.Viewport as py4godot_viewport
-import py4godot.classes.SceneTree.SceneTree as py4godot_scenetree
-import py4godot.classes.Window.Window as py4godot_window
+from py4godot.classes.Node import Node
+import py4godot.classes.Viewport as py4godot_viewport
+import py4godot.classes.SceneTree as py4godot_scenetree
+import py4godot.classes.Window as py4godot_window
 
 
 def get_viewport(node: Node) -> py4godot_viewport.Viewport: pass
@@ -12,4 +12,4 @@ def get_window(node: Node) -> py4godot_window.Window: pass
 
 def get_tree(node: Node) -> py4godot_scenetree.SceneTree: pass
 
-def get_singleton(name:str) -> object: pass
+def get_singleton(name: str) -> object: pass
