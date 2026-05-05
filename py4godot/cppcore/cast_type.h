6a402f2029fddf354aaded0ce7a3950dbe331d5c
niklas <niklas-zimmer@hotmail.com>
2026-05-05 22:21:16 +0200

    Fixed constexpr

--- py4godot/cppcore/cast_type.h
+++ py4godot/cppcore/cast_type.h
@@ -5,7 +5,7 @@ LIBRARY_API PyObject* cast_to_type(char* classname, PyObject* object_to_cast);
 LIBRARY_API void init_casting();
 
 
-constexpr unsigned int str2int(const char* str, int h = 0)
+inline constexpr unsigned int str2int(const char* str, int h = 0)
 {
     return !str[h] ? 5381 : (str2int(str, h+1) * 33) ^ str[h];
 }
