f93deaffa1d5619807009e099a505cd666418c6f
Niklas Zimmer <niklas-zimmer@hotmail.com>
2026-08-05 18:39:37 +0200

    Revert "Fixed generation of LIBRARY_API again"
    This reverts commit e1b47d812df76bc8fa7b17be1bf182dc5503a671.

--- generation_files/generate_classes_hpp.py
+++ generation_files/generate_classes_hpp.py
@@ -415,14 +415,19 @@ def generate_common_methods(class_):
     result = generate_newline(result)
     return result
 
+def generate_library_api(is_core):
+    if not is_core:
+        return "LIBRARY_API"
+    return ""
+
 def generate_switch_methods(is_core):
     res = ""
     res += f"{INDENT}virtual PyObject* switch_call_return(int method_hash, PyObject* args_tuple);"
     res = generate_newline(res)
-    res += f"{INDENT}static PyObject* call_constructor(int constructor_id, PyObject* args_tuple);"
+    res += f"{INDENT}{generate_library_api(is_core)} static PyObject* call_constructor(int constructor_id, PyObject* args_tuple);"
     res = generate_newline(res)
 
-    res += f"{INDENT}static PyObject* call_static_method_with_return(int method_hash, PyObject* args_tuple);"
+    res += f"{INDENT}{generate_library_api(is_core)} static PyObject* call_static_method_with_return(int method_hash, PyObject* args_tuple);"
     res = generate_newline(res)
     return res
 
